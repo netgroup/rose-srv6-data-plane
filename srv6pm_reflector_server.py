9c0934e15fb19893dd0cd30c96390c55763647b3
Gianluca Ligresti <ligrestigianluca@gmail.com>
2020-03-08 11:01:21 +0100

    modifiche effettuate

--- srv6pm_reflector_server.py
+++ srv6pm_reflector_server.py
@@ -4,9 +4,6 @@ import grpc
 import logging
 import srv6pmReflector_pb2
 import srv6pmReflector_pb2_grpc
-import srv6pmCommons_pb2
-import srv6pmCommons_pb2_grpc
-
 
 
 class ReflectorServicer(srv6pmReflector_pb2_grpc.SRv6PMReflectorServiceServicer):
@@ -15,28 +12,23 @@ class ReflectorServicer(srv6pmReflector_pb2_grpc.SRv6PMReflectorServiceServicer)
         self.port_server = 1234
 
     def startExperiment(self, request, context):
-
-        return srv6pmReflector_pb2.StartExperimentReflectorReply(status=200)
-
-
-###############################################################################
+        
+        return srv6pmReflector_pb2.StartExperimentReflectorReply(status=15)
 
     def stopExperiment(self, request, context):
         
-        return srv6pmReflector_pb2.StopExperimentReply(status=0)
+        return srv6pmReflector_pb2.StopExperimentReflectorReply(status=0)
 
     def retriveExperimentResults(self, request, context):
         
-        return srv6pmReflector_pb2.ExperimentDataResponse(status=0)
-
-################################################################################
+        return srv6pmReflector_pb2.RetriveExperimentDataReply(status=0)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     srv6pmReflector_pb2_grpc.add_SRv6PMReflectorServiceServicer_to_server(
         ReflectorServicer(), server)
-    server.add_insecure_port('localhost:50051')
+    server.add_insecure_port('127.0.0.1:50051')
     server.start()
     server.wait_for_termination()
 
--- srv6pm_sender_server.py
+++ srv6pm_sender_server.py
@@ -4,9 +4,6 @@ import grpc
 import logging
 import srv6pmSender_pb2
 import srv6pmSender_pb2_grpc
-import srv6pmCommons_pb2
-import srv6pmCommons_pb2_grpc
-
 
 
 class SenderServicer(srv6pmSender_pb2_grpc.SRv6PMSenderServiceServicer):
@@ -15,28 +12,23 @@ class SenderServicer(srv6pmSender_pb2_grpc.SRv6PMSenderServiceServicer):
         self.port_server = 1234
 
     def startExperiment(self, request, context):
-
-        return srv6pmSender_pb2.StartExperimentSenderReply(status=250)
-
-
-###############################################################################
+        
+        return srv6pmSender_pb2.StartExperimentSenderReply(status=19)
 
     def stopExperiment(self, request, context):
         
-        return srv6pmSender_pb2.StopExperimentReply(status=0)
+        return srv6pmSender_pb2.StopExperimentSenderReply(status=0)
 
     def retriveExperimentResults(self, request, context):
         
-        return srv6pmSender_pb2.ExperimentDataResponse(status=0)
-
-################################################################################
+        return srv6pmSender_pb2.RetriveExperimentDataReply(status=0)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     srv6pmSender_pb2_grpc.add_SRv6PMSenderServiceServicer_to_server(
         SenderServicer(), server)
-    server.add_insecure_port('localhost:50052')
+    server.add_insecure_port('127.0.0.1:50052')
     server.start()
     server.wait_for_termination()
 
